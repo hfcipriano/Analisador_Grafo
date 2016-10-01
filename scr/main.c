f228d717f9f7154df2aeb93cfe5a2303e6c0559e
Henrique Cipriano <henriquefc@hotmail.com.br>
2016-10-01 13:40:25 -0300

    Inclusão de exemplos cíclicos e áciclicos

--- scr/main.c
+++ scr/main.c
@@ -10,24 +10,66 @@ extern node *newNode(void*);
 extern vertice *newVertice(char*);
 extern aresta *newAresta(vertice*, vertice*);
 
+void exemploGrafoAciclico();
+void exemploGrafoCiclico();
+
 int main(){
+    printf("1 equivale à VERDADEIRO; 2 equivale à FALSO\n\n");
+
+    exemploGrafoAciclico();
+    exemploGrafoCiclico();
+
+    return 0;
+}
+
+void exemploGrafoAciclico(){
+    printf("Grafo Aciclico\n");
     grafo *g = newGrafo();
 
-    g->addAresta(g, newNode(newAresta(newVertice("a"), newVertice("b"))));
-    g->addAresta(g, newNode(newAresta(newVertice("b"), newVertice("c"))));
-    g->addAresta(g, newNode(newAresta(newVertice("c"), newVertice("d"))));
-    g->addAresta(g, newNode(newAresta(newVertice("d"), newVertice("a"))));
+    g->addAresta(g, newNode(newAresta(newVertice("1"), newVertice("2"))));
+    g->addAresta(g, newNode(newAresta(newVertice("1"), newVertice("5"))));
+    g->addAresta(g, newNode(newAresta(newVertice("1"), newVertice("4"))));
+    g->addAresta(g, newNode(newAresta(newVertice("2"), newVertice("5"))));
+    g->addAresta(g, newNode(newAresta(newVertice("3"), newVertice("6"))));
+    g->addAresta(g, newNode(newAresta(newVertice("4"), newVertice("6"))));
+    g->addAresta(g, newNode(newAresta(newVertice("4"), newVertice("7"))));
+    g->addAresta(g, newNode(newAresta(newVertice("5"), newVertice("7"))));
+    g->addAresta(g, newNode(newAresta(newVertice("6"), newVertice("8"))));
+    g->addAresta(g, newNode(newAresta(newVertice("7"), newVertice("8"))));
 
-    aresta *aresta = newAresta(newVertice("a"), newVertice("d"));
+    aresta *aresta = newAresta(newVertice("1"), newVertice("8"));
 
     //Task 1-2
     printf("%d", g->existeCaminho(g, aresta));
 
     //Task 3
-    printf("\n%d", g->existeCiclo(g, newVertice("a")));
+    printf("\n%d", g->existeCiclo(g, newVertice("1")));
 
     //Task 4
     g->exibeCaminhos(g, aresta);
+}
 
-    return 0;
+void exemploGrafoCiclico(){
+    printf("Grafo Ciclico\n");
+    grafo *g = newGrafo();
+
+    g->addAresta(g, newNode(newAresta(newVertice("1"), newVertice("2"))));
+    g->addAresta(g, newNode(newAresta(newVertice("2"), newVertice("1"))));
+    g->addAresta(g, newNode(newAresta(newVertice("2"), newVertice("3"))));
+    g->addAresta(g, newNode(newAresta(newVertice("2"), newVertice("5"))));
+    g->addAresta(g, newNode(newAresta(newVertice("3"), newVertice("4"))));
+    g->addAresta(g, newNode(newAresta(newVertice("4"), newVertice("2"))));
+    g->addAresta(g, newNode(newAresta(newVertice("4"), newVertice("5"))));
+    g->addAresta(g, newNode(newAresta(newVertice("4"), newVertice("6"))));
+
+    aresta *aresta = newAresta(newVertice("1"), newVertice("2"));
+
+    //Task 1-2
+    printf("%d", g->existeCaminho(g, aresta));
+
+    //Task 3
+    printf("\n%d", g->existeCiclo(g, newVertice("5")));
+
+    //Task 4
+    g->exibeCaminhos(g, aresta);
 }
